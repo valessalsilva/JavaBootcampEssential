2d523ec674d3571fdd0a5abf129bb4cafeace71d
valessa lopes <valessalopes.silva@gmail.com>
2025-12-12 14:24:34 -0300

    Initial commit for the Java project

---
+++ HELLO/src/App.java
@@ -0,0 +1,7 @@
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        System.out.println("configurando a jdk na versão" + args[0]);
+        System.out.println("Programando em " + args[1]);
+    }
+}
